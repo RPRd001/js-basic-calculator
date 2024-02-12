5adc27e7ec9d0fa0bc3416d10f256383238f52c6
Ra <132078939+RPRd001@users.noreply.github.com>
2024-02-12 13:36:53 -0500

    Added classes to the buttons so the css doesn't mess with the js functionality due to same class name. Also changed the js code in main.js for connecting the front end to the backend functionality -Now the calculations can be done using the UI.

--- main.js
+++ main.js
@@ -12,47 +12,100 @@
     1. Connect Front-End & Back-End
 */
 
+// Declaring/Initializing UI elements
+const numbers = document.querySelectorAll('.num-button')
+const operators = document.querySelectorAll('.operator-button')
+const calculatorScreen = document.querySelector('.calculator-screen')
+const clearButton = document.querySelector('.ce-button')
+
+let calculationTense = "";
+
+
 // Operations
 function addition(num1, num2) {
     let additionResult = num1 + num2;
-    return `The Result of the Sum is: ${additionResult}`
+    calculatorScreen.innerText = `${additionResult}`
 }
 
 function subtraction(num1, num2) {
     let subtractionResult = num1 - num2;
-    return `The Result of the subtraction is: ${subtractionResult}`
+    calculatorScreen.innerText = `${subtractionResult}`
 }
 
 function multiplication(num1, num2) {
     let multiplicationResult = num1 * num2;
-    return `The Result of the multiplication is: ${multiplicationResult}`
+    calculatorScreen.innerText = `${multiplicationResult}`
 }
 
 function division(num1, num2) {
     let divisionResult = num1 / num2;
-    return `The Result of the division is: ${divisionResult}`
+    calculatorScreen.innerText = `${divisionResult}`
 }
 
 // Main Function
-function calculate() {
-    let num1 = parseFloat(prompt("Introduce a number:"))
-    let num2 = parseFloat(prompt("Enter a 2nd Number:"))
-    let operand = prompt("Type the operation symbol (+,-,/,*)")
-
+function calculate(num1, num2, operand) {
     if (operand === "+") {
         addition(num1, num2)
     }
     else if (operand === "-") {
         subtraction(num1, num2)
     }
-    else if (operand === "*") {
+    else if (operand === "x") {
         multiplication(num1, num2)
     }
     else if (operand === "/") {
         division(num1, num2)
     }
     else {
-        console.log("You made a mistake while typing, refresh the page and try again.")
+        alert("You made a mistake while typing, refresh the page and try again.")
     }
     
 }
+
+function calculatorOS() {    
+    operators.forEach(operator => {
+        operator.addEventListener('click', () => {
+
+            if (operator.innerText == 'ON') {
+                calculatorScreen.classList.toggle('screen-off')
+            }
+            
+            if (operator.innerText == '=') {
+                // Need to decompose it into an array, then loop through it and take num1, num2 and operator. Then call calculate(num1, num2, operand)
+
+                calcArray = calculationTense.split(" ")
+
+                let num1 = calcArray[0];
+                let operand = calcArray[1];
+                let num2 = calcArray[2];
+
+                calculate(parseFloat(num1), parseFloat(num2), operand)
+
+                calculationTense = ""
+
+            }
+            else if (operator.innerText == 'CE') {
+                calculationTense = "";
+                calculatorScreen.innerText = `${calculationTense}`
+            }
+            else if (operator.innerText !== 'CE' && operator.innerText !== 'ON') {
+                calculationTense += ` ${operator.innerText} `
+                // Delete line below
+                console.log(calculationTense)
+                calculatorScreen.innerText = `${calculationTense}`
+            }
+        })
+    })
+
+    numbers.forEach(num => {
+        num.addEventListener('click', () => {
+            calculationTense += `${num.innerText}`
+            console.log(calculationTense)
+            calculatorScreen.innerText = `${calculationTense}`
+        })
+    });
+
+    
+}
+
+calculatorOS()
